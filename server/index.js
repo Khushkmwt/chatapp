54751f315c8757c325fb7cee0c227058c2472e2f
Dilkhush Kumawat <dilkhushkumawat33@gmail.com>
2025-12-05 19:06:02 +0530

    ..

--- server/index.js
+++ server/index.js
@@ -19,18 +19,10 @@ app.use(cors({
     credentials: true
 }));
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", process.env.ORIGIN); // Must match Vercel's URL
-//     res.header("Access-Control-Allow-Credentials", "true");       // Required for cookies
-//     res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH,OPTIONS");
-//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    
-//     if (req.method === "OPTIONS") {
-//         return res.sendStatus(200); // Properly handle preflight
-//     }
-
-//     next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.use("/uploads/profiles",express.static("uploads/profiles"))
 app.use("/uploads/files",express.static("uploads/files"))
